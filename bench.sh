115702561270d1ae38f8f11a6437425cb98d32a1
Jatin Agarwal <jatin.agarwal11@gmail.com>
2014-04-22 15:00:12 +0530

    completed extract function to generate csv file

--- bench.sh
+++ bench.sh
@@ -3,35 +3,45 @@
 #script to generate report for virtual lab server
 #Note that first parameter to this is script file containing all the urls
 #setting parameters for number of request n# and concurrency c# as global parameters
-n1=100
-c1=20
-n2=1000
-c2=400
 
 #function to create to get results for all urls i
 loadTest() {
-while read p; do 								#Entering loop reading url in $p from url file in each iteration. 
-  echo "Running tests for $p"					
-  q=$(echo $p | sed 's/[:/.-]/_/g') 			#getting file name by replacing following sysmbols from url /:.- and getting valid directory name
-  mkdir $q 										#creating directory for each url
-  cd $q											#opening directory
-  echo "ab -n $n1 -c $c1 -r $p"
-  ab -n $n1 -c $c1 -r $p > $q$n1$c1				#running ab tool for current url with first set of parameters
-
-  echo "ab -n $n2 -c $c2 -r $p"
-  ab -n $n2 -c $c2 -r $p > $q$n2$c2             #running ab too for current url with second set of parameters
-  cd ..											#Exiting current $q
-done < $1;										#Exiting loop
+while read p; do 
+  echo "Running tests for $p"         
+  q=$(echo $p | sed 's/[:/.-]/_/g')
+  mkdir $q
+  while read n c; do                   
+    cd $q 									
+    echo "ab -n $n -c $c $p"
+    ab -n $n -c $c $p > $q$n$c
+    cd ..
+  done < $2
+done < $1
 }
 
 #function to extract required fields reports previously generated
 extract() {
-
+#echo "I am the extract function for generating reports and I do nothing, I just exist"
+while read p; do
+  #echo "Running tests for $p" 
+  csv="_csv.csv"
+  res="No of Requests,Concurreny Level,Time taken for tests,Requests per second,Complete requests,Failed requests,Time per request(mean across all concurrent requests),Transfer rate"$'\n'       
+  q=$(echo $p | sed 's/[:/.-]/_/g') 
+  while read n c; do
+    res="$res$n,$c"                     
+    while read field; do               
+      cd $q
+      #echo "ab -n $n -c $c $p $field :"
+      res="$res,$(cat $q$n$c | grep "$field" | grep -o '[[:digit:]*\.[:digit:]]*')"
+      #res="$res,$tem"
+      cd ..
+    done < $3 
+    res="$res"$'\n'
+  done < $2 
+  echo "$res"
+done < $1         
 }
 
 #invoking funtions
-loadTest $1
-
-
-
-
+#loadTest $1 $2
+extract $1 $2 $3
